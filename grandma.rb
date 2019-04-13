cb3d685fa948828f753176db8f561a4ee430a562
Christopher Kalfas <chriskalfas@yahoo.com>
2019-04-13 17:02:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@ elsif arguement.upcase
   return "NO, NOT SINCE 1938!"
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
-else arguement = "I LOVE YOU GRANDMA!"
+else arguement == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
 end 
 end 
