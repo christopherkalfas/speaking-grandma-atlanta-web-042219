10b7d8270c14771dada061c043aac541fe98043f
Christopher Kalfas <chriskalfas@yahoo.com>
2019-04-13 16:59:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,6 +14,6 @@ return "NO, NOT SINCE 1938!"
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 else arguement = "I LOVE YOU GRANDMA!"
-  return "I LOVE YOU TOO PUMPKIN!"
+  puts  "I LOVE YOU TOO PUMPKIN!"
 end 
 end 
