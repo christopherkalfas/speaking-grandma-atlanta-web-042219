06a2baf713fefefcf5b2403cdda844aa5d9ea3d6
Christopher Kalfas <chriskalfas@yahoo.com>
2019-04-13 17:08:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,5 +3,6 @@ def speak_to_grandma(arguement)
   if arguement = arguement.upcase
     return "NO, NOT SINCE 1938!"
   elsif arguement = arguement.downcase
-    return 
+    return "HUH?! SPEAK UP, SONNY!"
+  else arguement = 
 end 
