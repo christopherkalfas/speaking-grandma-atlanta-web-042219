bd1130cce7f3e5f3e012721ce2ceb55f256a4ef5
Christopher Kalfas <chriskalfas@yahoo.com>
2019-04-13 17:01:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,16 +4,16 @@ def speak_to_grandma(arguement)
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 if arguement.downcase
-  puts "HUH?! SPEAK UP, SONNY!"
+  return "HUH?! SPEAK UP, SONNY!"
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
 elsif arguement.upcase
 
 # NO, NOT SINCE 1938!
-  puts "NO, NOT SINCE 1938!"
+  return "NO, NOT SINCE 1938!"
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 else arguement = "I LOVE YOU GRANDMA!"
-  puts  "I LOVE YOU TOO PUMPKIN!"
+  return "I LOVE YOU TOO PUMPKIN!"
 end 
 end 
